11041e65d8b8ae1298cac297a4b9d380ebf390cd
Hajax8 <t2luha01@students.oamk.fi>
2024-01-08 15:18:20 +0200

    tehty

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [age, setAge] = useState(25)
+  const [highrate, setHighrate] = useState('')
+  const [lowrate, setLowrate] = useState('') 
+
+function calculate() {
+  setLowrate( (220-age) *0.65 ) ;
+  setHighrate( (220-age) *0.85 );
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.container}>Age</Text>
+      <TextInput style={styles.field} value={age} onChangeText={text => setAge(text)} keyboardType='decimal-pad'/>
+      <Text>heartrate limits</Text>
+      <Text style={styles.field}>{lowrate.toFixed(0)} - {highrate.toFixed(0)}</Text>
+      <Button onPress={calculate} title="Calcultate"></Button>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop:50,
+    marginLeft: 10,
+    backgroundColor: 'lightblue',
   },
+  field: {
+    marginBottom: 10,
+    backgroundColor: 'white',
+  }
 });
